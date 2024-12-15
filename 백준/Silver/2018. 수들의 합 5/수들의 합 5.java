5efc433a80bd09963be4418cb29abc1cb4dd8a88
김성준 <88644396+sung-jun98@users.noreply.github.com>
2024-12-15 16:49:54 +0900

    [Silver V] Title: 수들의 합 5, Time: 148 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 public class Main {
 	static int N;
-	static int[] sum, num;
 	static int start, end;
 	static int answer;
 	static int tmpSum;
@@ -11,21 +10,12 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		num = new int[N + 1];
 
 		start = 1;
 		end = 1;
 		answer = 0;
 		tmpSum = 1;
-
 		// 입력끝
-
-		for (int i = 1; i <= N; i++) {
-			num[i] = i;
-		}
-
-		
-
 		while (end <= N) {
 			if (tmpSum > N) {
 				tmpSum -= start;
