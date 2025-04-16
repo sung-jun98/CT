487fbcc1910253512b67214a21797858a1ccc5eb
김성준 <88644396+sung-jun98@users.noreply.github.com>
2025-04-16 20:59:06 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,77 +1,73 @@
 
-import java.util.*;
+
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    static int N;
-    static int[][] map;
-    static boolean[][] visited;
-    static int cnt;
-    static ArrayList<Integer> cntArea;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
+public class Main {
+	static int N;
+	static int[][] map;
+	static boolean[][] visited;
+	static int dx[] = { -1, 0, 1, 0 };
+	static int dy[] = { 0, 1, 0, -1 };
+	static Deque<int[]> deque;
+	static ArrayList<Integer> answer;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        visited = new boolean[N][N];
-        cntArea = new ArrayList<>();
-        cnt = 0;
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		visited = new boolean[N][N];
+		answer = new ArrayList<>();
 
-        for(int i=0; i<N; i++){
-            String input = br.readLine();
-            for(int j=0; j<N; j++){
-                map[i][j] = input.charAt(j) - '0';
-            }
-        }
+		for (int i = 0; i < N; i++) {
+			String input = br.readLine();
+			for (int j = 0; j < N; j++) {
+				map[i][j] = input.charAt(j) - '0';
+			}
+		}
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                if(!visited[i][j] && map[i][j] == 1){
-                    bfs(i, j);
-                    cnt += 1;
-                }
-            }
-        }
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (visited[i][j] == false && map[i][j] == 1) {
+					bfs(i, j);
+				}
+			}
+		}
+		
+		Collections.sort(answer);
+		
+		System.out.println(answer.size());
+		for (int i = 0; i < answer.size(); i++) {
+			System.out.println(answer.get(i));
+		}
+	}
 
-        System.out.println(cnt);
-        Collections.sort(cntArea);
-        for(int answer : cntArea){
-            System.out.println(answer);
-        }
-    }
+	private static void bfs(int x, int y) {
+		deque = new ArrayDeque<>();
+		deque.offerLast(new int[] { x, y });
+		visited[x][y] = true;
+		int cnt = 1;
 
-    static void bfs(int x, int y) {
-        Deque<int[]> deque = new ArrayDeque<>();
-        int numHouse = 0;
-        visited[x][y] = true;
-        deque.offerLast(new int[] {x, y});
+		while (!deque.isEmpty()) {
+			int[] next = deque.pollFirst();
+			
 
-        while(!deque.isEmpty()){
-            int[] prevXY = deque.pollFirst();
-            int px = prevXY[0];
-            int py = prevXY[1];
-            numHouse += 1;
-            int nx, ny;
+			for (int i = 0; i < 4; i++) {
+				int nx = next[0] + dx[i];
+				int ny = next[1] + dy[i];
 
-            for(int i=0; i<4; i++){
-                nx = px + dx[i];
-                ny = py + dy[i];
+				if (isIn(nx, ny) && visited[nx][ny] == false && map[nx][ny] == 1) {
+					visited[nx][ny] = true;
+					deque.offerLast(new int[] { nx, ny });
+					cnt += 1;
+				}
+			}
+		}
+		answer.add(cnt);
+	}
 
-                if(isIn(nx, ny) && !visited[nx][ny]){
-                    if(map[nx][ny] == 1){
-                        deque.offerLast(new int[]{nx, ny});
-                        visited[nx][ny] = true;
-                    }
-                }
-            }
-        }
-        cntArea.add(numHouse);
-    }
-
-    static boolean isIn(int x, int y){
-        return x >= 0 && x < N && y >= 0 && y < N;
-    }
+	private static boolean isIn(int x, int y) {
+		return x >= 0 && x < N && y >= 0 && y < N;
+	}
 }
